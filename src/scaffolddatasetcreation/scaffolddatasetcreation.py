344028853a3d7f36fc0278503ef0185f7e02bd70
Kay <kayvvw@gmail.com>
2022-03-16 16:44:05 +1300

    Generate mesh exf file from input config json file

--- src/scaffolddatasetcreation/scaffolddatasetcreation.py
+++ src/scaffolddatasetcreation/scaffolddatasetcreation.py
@@ -1,16 +1,24 @@
 import argparse
 import os
+import json
+from opencmiss.zinc.context import Context
+
+from scaffoldmaker.scaffolds import Scaffolds
+from scaffoldmaker.scaffoldpackage import ScaffoldPackage
+
+import scaffoldmaker.scaffolds as sc
 
 
-def create_dataset(dataset_dir):
+def create_dataset(dataset_dir, mesh_config_file):
     create_folders(dataset_dir)
+    generate_mesh(mesh_config_file)
 
 def create_folders(path):
     folder_dir = []
-    folder_dir.append(os.path.join(path, "files", "derivative", "Scaffold"))
-    folder_dir.append(os.path.join(path, "files", "derivative"))
-    folder_dir.append(os.path.join(path, "files", "docs"))
-    folder_dir.append(os.path.join(path, "files", "primary"))
+    folder_dir.append(os.path.join(path, "derivative", "Scaffold"))
+    folder_dir.append(os.path.join(path, "derivative"))
+    folder_dir.append(os.path.join(path, "docs"))
+    folder_dir.append(os.path.join(path, "primary"))
     for filename in folder_dir:
         create_folder(filename)
 
@@ -22,16 +30,28 @@ def create_folder(path):
     else:
         print ("Successfully created the directory %s" % path)
 
+def generate_mesh(mesh_config_file):
+    context = Context("MeshGenerator")
+    region = context.createRegion()
+    f = open(mesh_config_file)
+    scaffoldConfig = json.load(f)
+    scaffoldType = scaffoldConfig["generator_settings"]["scaffoldPackage"]
+    scaffoldPackage = sc.Scaffolds_decodeJSON(scaffoldType)
+    scaffoldPackage.generate(region, applyTransformation=False)
+    file_name = "mesh.exf"
+    region.writeFile(file_name)
 
 def main():
     parser = argparse.ArgumentParser(description='Create a SPARC dataset.')
     parser.add_argument("dataset_dir", help='directory to create.')
+    parser.add_argument("mesh_config_file", help='mesh config json file to generate mesh exf file.')
 
     args = parser.parse_args()
     dataset_dir = args.dataset_dir
+    mesh_config_file = args.mesh_config_file
 
     # Create folders
-    create_dataset(dataset_dir)
+    create_dataset(dataset_dir, mesh_config_file)
 
 
 if __name__ == "__main__":
